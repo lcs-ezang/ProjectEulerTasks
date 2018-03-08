acfac0584d9c938c05e15c092790c18493cbbb3a
Zang Eric <ezang@lcs-mba3154.lcs.on.ca>
2018-03-08 09:09:34 -0500

    Finished Question 1

--- ProjectEuler.playground/Contents.swift
+++ ProjectEuler.playground/Contents.swift
@@ -3,3 +3,20 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+//Problem 1 Find sum of multiples of 3 and 5
+
+var total = 0
+let highNumber = 999
+
+for m in 1...highNumber {
+    if m%3 == 0 {
+    total += m
+} else if m%5 == 0 {
+    total += m
+} else {
+    
+    }
+}
+print(total)
+

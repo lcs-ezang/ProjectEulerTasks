db2a9bda10f4437a91b71d5e6a5518e2fc055d53
Zang Eric <ezang@lcs-mba3154.lan>
2018-03-11 01:08:15 -0500

    Finished Question 3

--- ProjectEuler.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ ProjectEuler.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -4,4 +4,21 @@ import Foundation
 
 var str = "Hello, playground"
 
+var highestFactor = 1
+var numberToDivide = 600851475143
+
+for m in stride(from: 2, to: numberToDivide - 1, by: 1){
+if numberToDivide%m == 0 {
+    numberToDivide = numberToDivide/m
+    highestFactor = m
+    print(highestFactor)
+    }
+}
+
+if highestFactor == 1 {
+        highestFactor = numberToDivide
+        print(highestFactor)
+}
+
+
 //: [Next](@next)
